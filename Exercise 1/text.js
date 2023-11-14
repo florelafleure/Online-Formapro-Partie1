35fc2b5d432491ad71dc453422eee1617b0da48b
ohugonnot <folken70@hotmail.com>
2023-11-14 11:14:49 +0100

    update dropdown

--- Exercise 1/text.js
+++ Exercise 1/text.js
@@ -1,21 +1,22 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function myFunction() {
 
-    console.log("Coucou")
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.drop')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
+// la fonction qui est déclenché par le clic sur le chevron
+function showDropdown() {
+  // selectionne ma liste de dropdown et switch la class show
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+// si je click n'importe sur le site
+window.onclick = function hideDropdownOnExternalClick(event) {
+  // et que je n'ai pas cliqué sur .drop
+  if (!event.target.matches(".drop")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      openDropdown.classList.remove("show");
     }
   }
+};
